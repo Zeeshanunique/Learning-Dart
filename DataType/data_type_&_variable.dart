2fdff51a33215fd78965079de9e77c62ec214d1a
Zeeshan Ali <zeeshanunique2619@gmail.com>
2023-02-11 20:38:01 +0530

    6th commit

--- DataType/data_type_&_variable.dart
+++ DataType/data_type_&_variable.dart
@@ -1,3 +1,16 @@
-void main(List<String> args) {
-  print("Fuck the bitches!!");
+///This is main function
+
+void main() {
+  int integer1 = 1;
+  print(integer1);
+  int integer2 = 2;
+  print(integer2);
+  integer1 = integer2;
+  print(integer2);
+
+//This is comment
+  var integer3;
+  integer3 = 3;
+  print(integer3);
+  print(integer3.runtimeType);
 }
